295806e19579fbf404bc9267ca8899354b41de32
GOKUL <GOKUL@192.168.10.4>
2019-08-15 14:44:39 +0530

    Queue using Array

---
+++ DS---ALGO/src/Queue_in_Array/Queue.java
@@ -0,0 +1,89 @@
+package Queue_in_Array;
+
+public class Queue {
+	int ar[];
+	int first=0;
+	int last=-1;
+	
+	Queue(int size)
+	{
+		ar=new int[size];
+	}
+	
+	void enQueue(int data)
+	{
+		if(last==ar.length-1)
+		{
+			System.out.println("Queue is Full Can't Insert");
+		}
+		else
+		{
+			last++;
+			ar[last]=data;
+		}
+	}
+	
+	void deQueue()
+	{
+		if(last==-1)
+		{
+			System.out.println("Queue is Empty");
+		}
+		else
+		{
+			System.out.println(ar[first]+" is deleted from the Queue");
+			first++;
+			if(first>last)
+			{
+				last=-1;
+				first=0;
+			}
+		}
+	}
+	
+	void peekInQueue()
+	{
+		if(last==-1)
+		{
+			System.out.println("Queue is Empty");
+		}
+		else
+		{
+			System.out.println(ar[first]);
+		}
+	}
+	
+	void display()
+	{
+		if(last==-1)
+		{
+			System.out.println("Queue is Empty Nothing to dispaly");
+		}
+		else
+		{
+			for(int i=first;i<=last;i++)
+			{
+				System.out.print(ar[i]+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	boolean isEmpty()
+	{
+		return (last==-1);
+	}
+	
+	boolean isFull()
+	{
+		return (last==ar.length-1);
+	}
+	
+	void deleteQueue()
+	{
+		ar=null;
+		last=-1;
+		first=0;
+	}
+
+}
